c466898ddb3cc298f76ee73029569df587c66765
S-Rad <shima.radfar@txs.de>
2020-03-30 15:11:36 +0200

    empty textfield after adding todo

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ window.onload = () => {
 const addtodolistener = () => {
   addtodo(addtodotextfield.value);
   renderalltodos();
+  addtodotextfield.value = null;
 };
 
 const addtodo = todotext => {
